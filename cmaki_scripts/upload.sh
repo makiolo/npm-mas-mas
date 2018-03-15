5691abcb4c4a46994b57e106db8d3b57d58b4514
Ricardo Marmolejo García <makiolo@gmail.com>
2018-03-15 22:42:10 +0100

    .

--- cmaki_scripts/upload.sh
+++ cmaki_scripts/upload.sh
@@ -9,5 +9,6 @@ export YMLFILE=$CMAKI_PWD/cmaki.yml
 # warning, TODO: detectar si hay cambios locales y avisar
 git diff $CMAKI_PWD
 
-cd $CMAKI_PWD/node_modules/cmaki_generator
+cd $CMAKI_PWD/node_modules/npm-mas-mas/cmaki_generator
 curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/make_artifact.sh | bash
+
