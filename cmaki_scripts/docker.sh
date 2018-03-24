a1a2a049ab1751aa365811b46114b4557c5bc070
Ricardo Marmolejo García <makiolo@gmail.com>
2018-03-24 13:14:00 +0100

    .

--- cmaki_scripts/docker.sh
+++ cmaki_scripts/docker.sh
@@ -9,11 +9,11 @@ docker run --rm makiolo/$IMAGE > ./dockcross-$IMAGE
 sed -e "s#DEFAULT_DOCKCROSS_IMAGE=dockcross/$IMAGE#DEFAULT_DOCKCROSS_IMAGE=makiolo/$IMAGE#g" dockcross-$IMAGE > makiolo-$IMAGE
 chmod +x ./makiolo-$IMAGE
 if [ "$PACKAGE" == "undefined" ]; then
-  # CI
-  ./makiolo-$IMAGE -a "-e MODE=$MODE -e NOCACHE_LOCAL=$NOCACHE_LOCAL -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=FALSE -e DEFAULT_DOCKCROSS_IMAGE=makiolo/$IMAGE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
+	# CI
+	./makiolo-$IMAGE -a "-e MODE=$MODE -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=FALSE -e DEFAULT_DOCKCROSS_IMAGE=makiolo/$IMAGE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/ci.sh | bash'
 else
-  # build package
-  ./makiolo-$IMAGE -a "-e MODE=$MODE -e NOCACHE_LOCAL=$NOCACHE_LOCAL -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=FALSE -e DEFAULT_DOCKCROSS_IMAGE=makiolo/$IMAGE -e PACKAGE=$PACKAGE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/make_artifact.sh | CMAKI_INSTALL=$(pwd)/bin bash'
+	# build package
+	./makiolo-$IMAGE -a "-e MODE=$MODE -e NOCACHE_REMOTE=$NOCACHE_REMOTE -e INSTALL_DEPENDS=FALSE -e DEFAULT_DOCKCROSS_IMAGE=makiolo/$IMAGE -e PACKAGE=$PACKAGE" bash -c 'curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/make_artifact.sh | CMAKI_INSTALL=$(pwd)/bin bash'
 fi
 error=$?
 
@@ -21,3 +21,4 @@ error=$?
 # docker rmi -f makiolo/$IMAGE
 
 exit $error
+
