9f2efd6e3b94c0146876cadd2cf043f236587440
Ricardo <makiolo@gmail.com>
2018-07-28 02:32:01 +0200

    Update make_artifact.sh

--- cmaki_scripts/make_artifact.sh
+++ cmaki_scripts/make_artifact.sh
@@ -10,10 +10,9 @@ if [ "$YMLFILE" == "undefined" ]; then
 		echo Error: must define env var YMLFILE or PACKAGE
 	else
 		echo building $PACKAGE ...
-		./build $PACKAGE --no-back-yaml --no-run-tests
+		./build $PACKAGE --no-back-yaml --no-run-tests -d
 	fi
 else
 	echo building from yaml file: ${YMLFILE} ...
-	./build --yaml=${YMLFILE} --no-run-tests
+	./build --yaml=${YMLFILE} --no-run-tests -d
 fi
-
