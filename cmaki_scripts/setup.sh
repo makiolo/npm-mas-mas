029348d8c0f0ea1e425606372b9a00944b0d874b
Ricardo Marmolejo García <makiolo@gmail.com>
2019-04-22 23:36:01 +0200

    .

--- cmaki_scripts/setup.sh
+++ cmaki_scripts/setup.sh
@@ -36,6 +36,10 @@ fi
 
 export WITH_CONAN=0
 if [ -f "$CMAKI_PWD/conanfile.txt" ] || [ -f "$CMAKI_PWD/conanfile.py" ]; then
+
+	# echo compile missing packages
+	conan install $CMAKI_PWD --build missing -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
+
 	echo conan install $CMAKI_PWD --build never -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
 	if ! conan install $CMAKI_PWD --build never -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}; then
 		echo Error conan
