74cc8afbd3a3d8cf3c7451c6ecb34ded3ec8885a
Ricardo <makiolo@gmail.com>
2019-05-17 11:12:58 -0700

    Update compile.cmd

--- cmaki_scripts/compile.cmd
+++ cmaki_scripts/compile.cmd
@@ -6,14 +6,9 @@ if "%Configuration%" == "Release" (
     set MODE=Debug
 )
 
-if "%Platform%" == "x64" (
-    set GENERATOR=Visual Studio 14 2015 Win64
-) else (
-    set GENERATOR=Visual Studio 14 2015
-)
-
 echo running in mode %MODE% ...
 cd %MODE%
 cmake --build . --config %MODE% --target install
 set lasterror=%errorlevel%
 cd ..
+exit /b %lasterror%
