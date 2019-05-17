af27e48d1a475f3be113b5b309772b97ccc86800
Ricardo <makiolo@gmail.com>
2019-05-17 11:49:31 -0700

    Update test.cmd

--- cmaki_scripts/test.cmd
+++ cmaki_scripts/test.cmd
@@ -6,12 +6,6 @@ if "%Configuration%" == "Release" (
     set MODE=Debug
 )
 
-if "%Platform%" == "x64" (
-    set GENERATOR=Visual Studio 14 2015 Win64
-) else (
-    set GENERATOR=Visual Studio 14 2015
-)
-
 echo running in mode %MODE% ...
 cd %MODE%
 ctest . --no-compress-output --output-on-failure -T Test -C %MODE% -V
