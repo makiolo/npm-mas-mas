4f0cd794219d04e5ead53a70252d07e83d0d0d29
Ricardo <makiolo@gmail.com>
2019-06-07 17:02:04 -0700

    Update setup.cmd

--- cmaki_scripts/setup.cmd
+++ cmaki_scripts/setup.cmd
@@ -26,15 +26,14 @@ conan install %CMAKI_PWD% --build missing -s build_type=%MODE%
 
 IF DEFINED Configuration (
     IF DEFINED Platform (
-        cmake .. -DCMAKE_MODULE_PATH=%CMAKI_PWD%\\node_modules\\npm-mas-mas\\cmaki -DCMAKE_BUILD_TYPE=%MODE% -G"%GENERATOR%" -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
+        cmake %CMAKI_PWD% -DCMAKE_MODULE_PATH=%CMAKI_PWD%/node_modules/npm-mas-mas/cmaki -DCMAKE_BUILD_TYPE=%MODE% -G"%GENERATOR%" -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
     ) ELSE (
-        cmake .. -DCMAKE_MODULE_PATH=%CMAKI_PWD%\\node_modules\\npm-mas-mas\\cmaki -DCMAKE_BUILD_TYPE=%MODE% -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
+        cmake %CMAKI_PWD% -DCMAKE_MODULE_PATH=%CMAKI_PWD%/node_modules/npm-mas-mas/cmaki -DCMAKE_BUILD_TYPE=%MODE% -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
     )
 ) ELSE (
-    cmake .. -DCMAKE_MODULE_PATH=%CMAKI_PWD%\\node_modules\\npm-mas-mas\\cmaki -DCMAKE_BUILD_TYPE=%MODE% -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
+    cmake %CMAKI_PWD% -DCMAKE_MODULE_PATH=%CMAKI_PWD%/node_modules/npm-mas-mas/cmaki -DCMAKE_BUILD_TYPE=%MODE% -DCMAKE_INSTALL_PREFIX=%CMAKI_INSTALL%
 )
 
 set lasterror=%errorlevel%
-cd ..
-
+cd %CMAKI_PWD%
 exit /b %lasterror%
