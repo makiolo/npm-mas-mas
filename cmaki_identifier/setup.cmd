9cf1267cb7bb1ad7c6061daeee15a13c9c76760c
Ricardo <makiolo@gmail.com>
2019-06-08 07:35:36 -0700

    Update setup.cmd

--- cmaki_identifier/setup.cmd
+++ cmaki_identifier/setup.cmd
@@ -4,10 +4,4 @@ if exist "boostorg_predef" (
 )
 git clone -q https://github.com/boostorg/predef.git boostorg_predef
 
-if exist "__setup.cmd" (
-	del __setup.cmd
-)
-
-curl -s https://raw.githubusercontent.com/makiolo/cmaki_scripts/master/setup.cmd > __setup.cmd
-call __setup.cmd
-del __setup.cmd
+..\cmaki_scripts\setup.cmd
