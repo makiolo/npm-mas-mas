3076146a83da0b5d0709af11c86b52c75d5f11dd
Ricardo <makiolo@gmail.com>
2019-06-07 19:43:45 -0700

    Update setup.cmd

--- cmaki_scripts/setup.cmd
+++ cmaki_scripts/setup.cmd
@@ -11,9 +11,11 @@ if "%Configuration%" == "Release" (
 if "%Platform%" == "x64" (
     :: set GENERATOR=Visual Studio 16 2019 Win64
     set GENERATOR=Visual Studio 14 2015 Win64
+    set ARCH=x86_64
 ) else (
     :: set GENERATOR=Visual Studio 16 2019
     set GENERATOR=Visual Studio 14 2015
+    set ARCH=x86
 )
 
 echo running in mode %MODE% ...
@@ -23,8 +25,12 @@ md %MODE%
 :: setup
 cd %MODE%
 
-conan install %CMAKI_PWD% --build missing -s build_type=%MODE%
-:: conan install %CMAKI_PWD% --build never -s build_type=%MODE%
+:: -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
+:: compiler=Visual Studio
+:: compiler.runtime=MD
+:: compiler.version=14
+
+conan install %CMAKI_PWD% --build missing -s build_type=%MODE% -s arch_build=%ARCH% -s compiler=Visual Studio
 
 IF DEFINED Configuration (
     IF DEFINED Platform (
