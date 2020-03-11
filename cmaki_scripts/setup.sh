1d0812bb73dbeddca3c94f3c2c99b160971a37ed
Ricardo <makiolo@gmail.com>
2020-03-12 00:58:51 +0100

    Update setup.sh

--- cmaki_scripts/setup.sh
+++ cmaki_scripts/setup.sh
@@ -37,8 +37,9 @@ fi
 export WITH_CONAN=0
 if [ -f "$CMAKI_PWD/conanfile.txt" ] || [ -f "$CMAKI_PWD/conanfile.py" ]; then
 
-	# echo compile missing packages
-	# conan install $CMAKI_PWD --build missing -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
+	if [ "$NPP_CI" == "FALSE" ]; then
+		conan install $CMAKI_PWD --build missing -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
+	fi
 
 	echo conan install $CMAKI_PWD --build never -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}
 	if ! conan install $CMAKI_PWD --build never -s compiler=${COMPILER} -s build_type=${MODE} -s compiler.libcxx=${COMPILER_LIBCXX} -s compiler.version=${COMPILER_VERSION}; then
